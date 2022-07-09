75033c67e7ceb9aa0dee66b0afb599aa49b076f8
Marek Michalik <marekmichalik43@gmail.com>
2022-07-10 01:24:34 +0200

    Change architecture

--- neural.py
+++ neural.py
@@ -9,9 +9,9 @@ class MarioNet(nn.Module):
         c, h, w = input_dim
 
         self.online = nn.Sequential(
-            nn.Conv2d(in_channels=c, out_channels=32, kernel_size=4, stride=2),
+            nn.Conv2d(in_channels=c, out_channels=64, kernel_size=4, stride=2),
             nn.ReLU(),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
             nn.ReLU(),
             nn.Flatten(),
             nn.Linear(3136, 512),
